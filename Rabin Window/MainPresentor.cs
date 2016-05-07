d2051be1bb2f8e783c6d53cc7a46233bd4afd6ef
Sammers21 <titantins@ya.ru>
2016-05-08 00:56:13 +0300

    add method Save

--- Rabin Window/MainPresentor.cs
+++ Rabin Window/MainPresentor.cs
@@ -13,11 +13,65 @@ namespace Rabin_Window
         private readonly IFileManager _manager;
         private readonly IMessageService _messageService;
 
+        private string _currentFilePath;
+
         public MainPresentor(IMainForm imainForm, IFileManager _manager, IMessageService _messageService)
         {
             this._imainForm = imainForm;
             this._manager = _manager;
             this._messageService = _messageService;
+
+            _imainForm.SetSymbolCount(0);
+            _imainForm.FileSaveClick += ImainForm_FileSaveClick;
+            _imainForm.FileOpenClick += ImainForm_FileOpenClick;
+            _imainForm.GoToMenuClick += ImainForm_GoToMenuClick;
+            _imainForm.ContentChanged += ImainForm_ContentChanged;
+        }
+
+        private void ImainForm_ContentChanged(object sender, EventArgs e)
+        {
+            _imainForm.SetSymbolCount(_imainForm.Content.Length);
+        }
+
+        private void ImainForm_GoToMenuClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ImainForm_FileOpenClick(object sender, EventArgs e)
+        {
+            try
+            {
+                string filepath = _imainForm.FilePath;
+                bool isExist = _manager.isExist(filepath);
+
+                if (!isExist)
+                {
+                    _messageService.ShowExclamation("Такого файлай не сушествует");
+                    return;
+                }
+
+                _currentFilePath = filepath;
+
+                //ToDo проверка ключей
+
+                string Content = _manager.GetContent(_currentFilePath, _imainForm.SecretKeyOne, _imainForm.SecretKeyTwo);
+
+                int count = _manager.GetSymbloCount(Content);
+
+                _imainForm.Content = Content;
+                _imainForm.SetSymbolCount(count);
+
+            }
+            catch (Exception ex)
+            {
+                _messageService.ShowError(ex.Message);
+            }
+        }
+
+        private void ImainForm_FileSaveClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
